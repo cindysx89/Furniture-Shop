4d757aeefdae53eb373e29238d9daa1e905120b3
Cindy Xie <cindyxie89@gmail.com>
2018-07-23 11:36:57 +0100

    changed seeds slightly and working on product & manuf. controller's CRUD actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @manufacturer = Manufacturer.find(params[:manufacturer_id])
     @product = Product.find(params[:id])
   end
 
